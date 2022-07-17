6917b8088ae94c34cbf68db8077043845f9f1f6d
Han Pham <hanpham@Hans-MacBook-Pro.local>
2022-07-16 20:56:26 -0700

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+
+// Import pages
+import Home from './pages/Home';
+
+// Import components
+import Navbar from './components/Navbar';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ToastContainer 
+        position='top-right'
+        autoClose={3000}
+        closeOnClick
+        draggable
+        pauseOnHover
+        theme='dark'
+      />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function Home() {
+    
+    const notify = () => toast("Hello World!");
+
+    return(
+        <div>
+            Home
+            <button onClick={notify}>Notify!</button>
+            <ToastContainer 
+                position='top-right'
+                autoClose={3000}
+                closeOnClick
+                draggable
+                pauseOnHover
+                theme='dark'
+            />
+        </div>
+    )
+}
+
